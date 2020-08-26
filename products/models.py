996b275ac1074c14622ae02313b288dd9d06bc41
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-08-26 13:06:42 +0000

    Added products app, models and categories and products fixtures

---
+++ products/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    viewer_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def find_viewer_name(self):
+        return self.viewer_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey('Category', null=True,
+                                 blank=True, on_delete=models.SET_NULL)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
