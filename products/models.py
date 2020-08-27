2bae9ab33d9ee2b24869d83943a919a59f8c2235
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-08-27 15:22:11 +0000

    Added more products to fixture and changed product model to include quantity

--- products/models.py
+++ products/models.py
@@ -19,9 +19,12 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    # Sets default price to all products that require budget
     category = models.ForeignKey('Category', null=True,
                                  blank=True, on_delete=models.SET_NULL)
+    quantity = models.IntegerField(default=1)
+    # Sets default quantity to all cakes not sold as pack
     image = models.ImageField(null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
 
