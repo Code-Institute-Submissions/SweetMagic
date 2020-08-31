0933a5ae7b8c8f46b9efb6618409cc4216f97dc6
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-08-31 13:30:05 +0000

    Added product detail template and found error on products  image urls

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -12,3 +12,15 @@ def products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product(request, product_id):
+    """ A view to display a specific product and its details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product.html', context)
