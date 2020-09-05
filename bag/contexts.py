cbd1ea6a8a0102bf64e69238f40a02facb2564c7
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-09-05 16:41:49 +0000

    Removed cupcake cream flavour choice and topping choice and added product flavour logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
 
     bag_items = []
@@ -8,15 +9,27 @@ def bag_contents(request):
     number_of_products = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        number_of_products += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            number_of_products += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for flavour, quantity in item_data['items_by_flavour'].items():
+                total += quantity * product.price
+                number_of_products += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'flavour': flavour,
+                })
 
     context = {
         'bag_items': bag_items,
--- bag/views.py
+++ bag/views.py
@@ -8,12 +8,24 @@ def my_bag(request):
 def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    flavour = None
+    if 'flavour' in request.POST:
+        flavour = request.POST['flavour']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if flavour:
+        if item_id in list(bag.keys()):
+            if flavour in bag[item_id]['items_by_flavour'].keys():
+                bag[item_id]['items_by_flavour'][flavour] += quantity
+            else:
+                bag[item_id]['items_by_flavour'][flavour] = quantity
+        else:
+            bag[item_id] = {'items_by_flavour': {flavour: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
