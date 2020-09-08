722500c3ca80f7bd8e32b7c9498296ae3ba145f3
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-09-08 14:41:40 +0000

    Added more toasts to my views

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from products.models import Product
 
@@ -8,7 +8,7 @@ def my_bag(request):
 
 
 def add_to_bag(request, item_id):
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     flavour = None
@@ -20,13 +20,17 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if flavour in bag[item_id]['items_by_flavour'].keys():
                 bag[item_id]['items_by_flavour'][flavour] += quantity
+                messages.success(request, f'Updated {product.name} with {flavour} quantity to {bag[item_id]["items_by_flavour"][flavour]}')
             else:
                 bag[item_id]['items_by_flavour'][flavour] = quantity
+                messages.success(request, f'Added {product.name} with {flavour} to your bag')
         else:
             bag[item_id] = {'items_by_flavour': {flavour: quantity}}
+            messages.success(request, f'Added {product.name} with {flavour} to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
@@ -36,7 +40,9 @@ def add_to_bag(request, item_id):
 
 
 def update_bag(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
     flavour = None
     if 'flavour' in request.POST:
         flavour = request.POST['flavour']
@@ -45,32 +51,46 @@ def update_bag(request, item_id):
     if flavour:
         if quantity > 0:
             bag[item_id]['items_by_flavour'][flavour] = quantity
+            messages.success(request, f'Updated {product.name} with {flavour} quantity to {bag[item_id]["items_by_flavour"][flavour]}')
         else:
             del bag[item_id]['items_by_flavour'][flavour]
             if not bag[item_id]['items_by_flavour']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} with {flavour} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
-    return redirect(reverse('my_bag'))
+    return redirect(redirect_url)
 
 
 def delete_from_bag(request, item_id):
-    flavour = None
-    if 'flavour' in request.POST:
-        flavour = request.POST['flavour']
-    bag = request.session.get('bag', {})
 
-    if flavour:
-        del bag[item_id]['items_by_flavour'][flavour]
-        if not bag[item_id]['items_by_flavour']:
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        redirect_url = request.POST.get('redirect_url')
+        flavour = None
+        if 'flavour' in request.POST:
+            flavour = request.POST['flavour']
+        bag = request.session.get('bag', {})
+
+        if flavour:
+            del bag[item_id]['items_by_flavour'][flavour]
+            if not bag[item_id]['items_by_flavour']:
+                bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} with {flavour} from your bag')
+        else:
             bag.pop(item_id)
-    else:
-        bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
-    request.session['bag'] = bag
-    return redirect(reverse('my_bag'))
+        request.session['bag'] = bag
+        return redirect(redirect_url)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
