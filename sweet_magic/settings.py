a546d47bc31a313f6b48a4711da66b866497d533
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-09-11 16:10:40 +0000

    Added initial checkout functionality

--- sweet_magic/settings.py
+++ sweet_magic/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    'crispy_forms'
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -167,3 +167,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Stripe
+
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
