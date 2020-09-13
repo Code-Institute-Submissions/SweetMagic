509ee21ddfd12902023927e60a1f841af1e63aab
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-09-13 10:51:34 +0000

    Added profile form

---
+++ profiles/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import UserProfile
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_street_address': 'Street Address',
+            'default_post_code': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe_style_input'
+            self.fields[field].label = False
